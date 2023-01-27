68221bbc667b09c6b2459510191f442bc111b705
mdqvic <mdqvic@inf.elte.hu>
2023-01-27 14:30:23 +0400

    push

--- HW3/utils.py
+++ HW3/utils.py
@@ -19,24 +19,25 @@ def ply2df(path):
     return pts2df(np.column_stack((x, y, z)))
 
 
-def rotate_point_cloud(vertex_data, rotation, azimuth):
-    # Convert rotation and azimuth to radians
-    rotation = np.radians(rotation)
-    azimuth = np.radians(azimuth)
+def rotate_point_cloud(angle_x, angle_y, angle_z):
+    x = np.radians(angle_x)
+    y = np.radians(angle_y)
+    z = np.radians(angle_z)
 
-    # Define the rotation matrices
-    R_x = np.array([[1, 0, 0],
-                    [0, np.cos(rotation), -np.sin(rotation)],
-                    [0, np.sin(rotation), np.cos(rotation)]])
-    R_y = np.array([[np.cos(azimuth), 0, np.sin(azimuth)],
-                    [0, 1, 0],
-                    [-np.sin(azimuth), 0, np.cos(azimuth)]])
+    Rx = np.array([[1, 0, 0],
+                   [0, np.cos(x), -np.sin(x)],
+                   [0, np.sin(x), np.cos(x)]])
 
-    # Rotate the point cloud
-    rotated_vertex_data = np.dot(vertex_data, R_y.T)
-    rotated_vertex_data = np.dot(rotated_vertex_data, R_x.T)
+    Ry = np.array([[np.cos(y), 0, np.sin(y)],
+                   [0, 1, 0],
+                   [-np.sin(y), 0, np.cos(y)]])
 
-    return pts2df(rotated_vertex_data)
+    Rz = np.array([[np.cos(z), -np.sin(z), 0],
+                   [np.sin(z), np.cos(z), 0],
+                   [0, 0, 1]])
+
+    R = np.dot(Rz, np.dot(Ry, Rx))
+    return R
 
 
 def so3_rotation(vertex_data, rotation_vector):
@@ -59,10 +60,12 @@ def rotation_matrix_to_rotation_vector(R):
     return -rotation_vector
 
 
-def add_noise_rotate_translate_pts(vertex_data, noise_std=0.01, translation_range=10):
+def add_noise_rotate_translate_pts(vertex_data, noise_std=0.01, degrees=[45, 45, 45], translation_range=10):
     noisy_vertex_data = vertex_data + np.random.normal(0, noise_std, vertex_data.shape)
 
-    rotation_vector = (2 * np.random.rand(3) - 1) * 2 * np.pi
+    R = rotate_point_cloud(degrees[0], degrees[1], degrees[2])
+
+    rotation_vector = rotation_matrix_to_rotation_vector(R)
     rotated_vertex_data = so3_rotation(noisy_vertex_data, rotation_vector)
 
     translation_vector = [np.random.uniform(-translation_range, translation_range) for _ in range(3)]
@@ -120,7 +123,7 @@ def icp(original_pts, modified_pts, max_iterations=50, tolerance=1e-10):
         if np.abs(e - prev_e) < tolerance:
             break
 
-    return original_pts, R, t
+    return original_pts
 
 
 def trimmed_icp(original_pts, modified_pts, max_iterations=50, tolerance=1e-10, threshold=0.1):
@@ -143,45 +146,41 @@ def trimmed_icp(original_pts, modified_pts, max_iterations=50, tolerance=1e-10, 
         if np.abs(e - prev_e) < tolerance:
             break
 
-    return original_pts, R, t
+    return original_pts
+
+
+'''
+------- QUATERNION -----------
+Find the centroid of both point clouds.
+Subtract the centroid from all points in both point clouds.
+Compute the correlation matrix H between the two point clouds.
+Compute the quaternion that represents the rotation by taking the eigenvector of H corresponding to the largest eigenvalue.
+Convert the quaternion to a rotation matrix.
+'''
 
 
 def quaternion_estimate_transformation(source_points, target_points):
     # Estimate the centroids of the point clouds
     source_centroid = np.mean(source_points, axis=0)
     target_centroid = np.mean(target_points, axis=0)
-
     # Subtract the centroids from the point clouds
     centered_source_points = source_points - source_centroid
     centered_target_points = target_points - target_centroid
-
-    print(centered_target_points.shape, centered_source_points.shape)
     # Compute the cross-covariance matrix
     H = centered_target_points.T @ centered_source_points
-
     # Compute the quaternion that maximizes the trace of H
-    quat = np.linalg.eig(H)[1][:, np.argmax(np.linalg.eig(H)[0])]
 
+    quat = np.linalg.eig(H)[1][:, np.argmax(np.linalg.eig(H)[0])]
     # Normalize the quaternion
     quat /= np.linalg.norm(quat)
-
     # Compute the rotation matrix from the quaternion
-    R = quat_to_rotation_matrix(quat)
+    return quat
 
+    R = (quat)
     # Compute the translation from the centroids
     t = target_centroid - R @ source_centroid
-
     # Return the transformation as a 4x4 matrix
-    T = np.eye(4)
-    T[:3, :3] = R
-    T[:3, 3] = t
-    return T
-
-
-def quat_to_rotation_matrix(q):
-    # Compute the rotation matrix from the quaternion
-    q = q / np.linalg.norm(q)
-    x, y, z, w = q
-    return np.array([[1 - 2 * y * y - 2 * z * z, 2 * x * y - 2 * z * w, 2 * x * z + 2 * y * w],
-                     [2 * x * y + 2 * z * w, 1 - 2 * x * x - 2 * z * z, 2 * y * z - 2 * x * w],
-                     [2 * x * z - 2 * y * w, 2 * y * z + 2 * x * w, 1 - 2 * x * x - 2 * y * y]])
+    return R, t
+    #
+    #
+    # Compute the rotation matrix from the quaternion    q = q / np.linalg.norm(q)    x, y, z, w = q    return np.array([[1 - 2 * y * y - 2 * z * z, 2 * x * y - 2 * z * w, 2 * x * z + 2 * y * w],                     [2 * x * y + 2 * z * w, 1 - 2 * x * x - 2 * z * z, 2 * y * z - 2 * x * w],                     [2 * x * z - 2 * y * w, 2 * y * z + 2 * x * w, 1 - 2 * x * x - 2 * y * y]])
